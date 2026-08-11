ec5a8b5abb9909d77a5e38f8f7fd2a5636a1ffec
Ashish Meena <ashishmeena35622@gmail.com>
2026-08-11 20:27:28 +0530

    did some dirty frontend works like started building the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { BrowserRouter, Link, redirect, Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar"
 import Login from "./pages/login"
+import { HoverBorderGradient } from "./components/ui/hover-border-gradient"
+
+
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="top-0 w-full h-full flex flex-col gap-3 items-center justify-center bg-black text-white">
+        <div className="text-6xl flex items-center justify-center font-semibold">Caption your  video easily with capvi</div>
+        <div>
+          <HoverBorderGradient as={"button"} children={<Link to="/captioning">Start Captioning</Link>}
+            onClick={() => {
+              console.log("clicked");
+              redirect("http://localhost:5173/captioning")
+            }} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+function Captioning(){
+  return(
+    <div className="w-full h-full flex items-center justify-center">
+      start captioning
+    </div>
+  )
+}
 
 
 function App() {
@@ -7,9 +38,20 @@ function App() {
   return (
     <>
       <main className="w-full h-screen bg-white text-black">
-        <Navbar/>
-        <Login/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}
+            ></Route>
 
+            <Route path="/login" element={<Login />}>
+            </Route>
+
+            <Route path="/captioning" element={<Captioning/>}>
+            </Route>
+
+            <Route path="/*" element={"404 page not found"}/>
+          </Routes>
+        </BrowserRouter>
       </main>
     </>
   )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CiUser } from "react-icons/ci";
 
 function Navbar() {
     const [user, setUser] = React.useState("Guest");
@@ -8,7 +9,7 @@ function Navbar() {
         <div className='w-full flex items-center justify-center h-20 sticky top-0 bg-black text-white'>
             <div className='w-11/12 m-auto flex items-center justify-around'>
                 <div>Video-Transcription</div>
-                <div>User: {user}</div>
+                <div className='rounded-full bg-red-100 p-1'> <CiUser className='text-black '/> </div>
             </div>
 
         </div>
